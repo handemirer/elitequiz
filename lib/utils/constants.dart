cc4564136120f0dc6eba8c8e3aa16403ebd2c267
Han Demirer <43323219+handemirer@users.noreply.github.com>
2022-04-15 13:14:36 +0300

    constants color

---
+++ lib/utils/constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor elitequizepallete =
+    MaterialColor(_elitequizepalletePrimaryValue, <int, Color>{
+  50: Color(0xFFECECFD),
+  100: Color(0xFFCECFFA),
+  200: Color(0xFFAEAFF7),
+  300: Color(0xFF8E8FF4),
+  400: Color(0xFF7577F1),
+  500: Color(_elitequizepalletePrimaryValue),
+  600: Color(0xFF5557ED),
+  700: Color(0xFF4B4DEB),
+  800: Color(0xFF4143E8),
+  900: Color(0xFF3032E4),
+});
+const int _elitequizepalletePrimaryValue = 0xFF5D5FEF;
+
+const MaterialColor elitequizepalleteAccent =
+    MaterialColor(_elitequizepalleteAccentValue, <int, Color>{
+  100: Color(0xFFFFFFFF),
+  200: Color(_elitequizepalleteAccentValue),
+  400: Color(0xFFC1C2FF),
+  700: Color(0xFFA7A8FF),
+});
+const int _elitequizepalleteAccentValue = 0xFFF4F4FF;
