0b81481e1878b44e6af662224b3e6c8b89bf4e60
Han Demirer <43323219+handemirer@users.noreply.github.com>
2022-04-15 15:51:27 +0300

    welcome

---
+++ lib/views/welcome.dart
@@ -0,0 +1,65 @@
+import 'package:elitequiz/utils/constants.dart';
+import 'package:elitequiz/views/widgets.dart';
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(child: eqLogo()),
+            Expanded(
+              child: Container(
+                width: double.maxFinite,
+                decoration: roundDecor(eqColor),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 32, right: 32, top: 8.0, bottom: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      eqText(
+                        "Welcome",
+                        size: 48,
+                        color: Colors.white,
+                      ),
+                      eqText(
+                        "Welcome the elite quiz,\njust enjoy.",
+                        size: 25,
+                        color: Colors.white,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          eqButton(
+                            title: 'Sign up',
+                            onPressed: () {},
+                            textColor: Colors.white,
+                            color: Colors.black,
+                          ),
+                          eqButton(
+                            title: 'Sign in',
+                            onPressed: () {},
+                            textColor: Colors.black,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/widgets.dart
@@ -0,0 +1,83 @@
+import 'package:elitequiz/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+AppBar eqAppBar(title) {
+  return AppBar(
+    title: Text(title),
+    elevation: 0,
+  );
+}
+
+Widget eqText(text, {double size = 14, Color color = Colors.black}) {
+  return Text(
+    text,
+    style: TextStyle(
+      fontSize: size,
+      color: color,
+    ),
+  );
+}
+
+Widget eqPadding(Widget widget, {double padding = 8}) {
+  return Padding(
+    padding: EdgeInsets.all(padding),
+    child: widget,
+  );
+}
+
+Widget eqLogo() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      SvgPicture.asset(
+        "assets/logo/logo.svg",
+        height: 96,
+      ),
+      Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SvgPicture.asset(
+            "assets/logo/elite.svg",
+            height: 48,
+          ),
+          SvgPicture.asset(
+            "assets/logo/quiz.svg",
+            height: 48,
+          ),
+        ],
+      )
+    ],
+  );
+}
+
+BoxDecoration roundDecor(Color color) {
+  return BoxDecoration(
+    color: color,
+    borderRadius: const BorderRadius.only(
+      topLeft: Radius.circular(32.0),
+      topRight: Radius.circular(32.0),
+    ),
+  );
+}
+
+Widget eqButton({
+  required String title,
+  VoidCallback? onPressed,
+  Color textColor = Colors.black,
+  Color color = eqColor,
+}) {
+  return ElevatedButton(
+    child: Text(
+      title,
+      style: TextStyle(
+        color: textColor,
+      ),
+    ),
+    onPressed: onPressed,
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      primary: color,
+    ),
+  );
+}
